6e77d580f6b390113802d4de9e0ff92cb1c786df
Your Name <dealvik@gmail.com>
2020-09-03 20:01:11 -0400

    more complex project added

--- Lab1/WordFileCounter/src/com/company/Main.java
+++ Lab1/WordFileCounter/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import javax.print.attribute.standard.JobKOctets;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws FileNotFoundException {
+        final String fileName = "sampleWords.txt";
+
+        Scanner input = new Scanner(new File(fileName));
+        Map<String, Integer> words = new TreeMap<>(); //<key> word <value> number of times occurred
+
+        while (input.hasNext()) {
+            String currentWord = input.next();
+            currentWord = currentWord.replaceAll("\\W", "");
+            currentWord = currentWord.replaceAll("\\d", "");
+            currentWord = currentWord.toLowerCase();
+
+            if(words.containsKey(currentWord)) {
+                // word does exist
+                words.put(currentWord, Integer.parseInt(words.get(currentWord).toString()) + 1);
+            } else {
+                // word doesn't exist
+                words.put(currentWord, 1);
+            }
+        }
+        input.close();
+
+        for (Object key : words.keySet()) {
+            Object currentVal = words.get(key);
+            System.out.println(key + " : " + currentVal);
+
+        }
     }
 }
